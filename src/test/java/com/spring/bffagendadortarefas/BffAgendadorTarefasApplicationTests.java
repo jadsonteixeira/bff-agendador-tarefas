233b949cddca7b68f2c25a3b49126464fdc5c824
Jadson Teixeira <jadsontxm@gmail.com>
2026-04-23 14:39:45 -0300

    commit inicial

---
+++ src/test/java/com/spring/bffagendadortarefas/BffAgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.bffagendadortarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffAgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
